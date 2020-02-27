a7beb7969a7f1824f650df95a5cb9f7761839705
Coinsenda <construyacol@gmail.com>
2020-02-27 09:34:21 -0500

    environment cleaned

--- src/environment/index.js
+++ src/environment/index.js
@@ -8,44 +8,9 @@ export const Environment = {
   DepositApiUrl:process.env.NODE_ENV === 'development' ? 'https://sendadeposit.ngrok.io/api/' : 'https://sendadeposit.ngrok.io/api/',
   WithdrawApiUrl:process.env.NODE_ENV === 'development' ? 'https://sendawithdraw.ngrok.io/api/' : 'https://sendawithdraw.ngrok.io/api/',
   SwapApiUrl:process.env.NODE_ENV === 'development' ? 'https://sendaswap.ngrok.io/api/' : 'https://sendaswap.ngrok.io/api/',
-  CountryUrl:'https://countrylistapi.ngrok.io/' //endpoint imagenes
+  CountryUrl:'https://countrylistapi.ngrok.io/'
 }
 
-// http://190.192.134.201:5900
+
 
 export default Environment
-// https://plecto.serveo.net
-// ApiUrl:process.env.NODE_ENV === 'development' ? 'http://192.168.0.16:3001/api/' : 'https://www.binance.com'
-// token coinsenda
-
-  // user: zeky.laf+local@gmail.com
-  // pw: starnet1
-
-// users
-  // 5bea1f01ba84493018b7528c
-  // 5bf1c8d9ff5b671145bc1e88
-
-// https://mensis.serveo.net/
-// https://archa.serveo.net/
-
-// https://oldauth.ngrok.io/public/signin?clientId=5d234981249f0d2d1bf1b72e
-
-// 5d234a113035be2e18a953ca
-
-// nuevas APIS
-
-// Country: https://countrylistapi.ngrok.io
-// Auth: https://oldauth.ngrok.io
-// Identity: https://sendaidentity.ngrok.io
-// Tx: https://sendatx.ngrok.io
-// Account: https://sendaaccount.ngrok.io
-// Deposit: https://sendadeposit.ngrok.io
-// Witdhraw: https://sendawithdraw.ngrok.io
-// Swap: https://sendaswap.ngrok.io
-
-// remote machine
-
-// depoosit: npm run npm2
-// swap: npm run nodemon
-
-// https://dev-admin.devsertec.com/ ==> deploy url staging
