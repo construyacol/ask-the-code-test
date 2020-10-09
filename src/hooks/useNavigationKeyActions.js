9ff33ea14cd3531903b2487598c55ab953d53ce2
douglasacost <douglasacostaaquino@gmail.com>
2020-10-09 15:29:21 -0400

    update useNavigation hook

--- src/hooks/useNavigationKeyActions.js
+++ src/hooks/useNavigationKeyActions.js
@@ -15,7 +15,7 @@ const DEFAULT_ARGS = {
  * @param {Object} config representa la configuracion y datos del hook
  * @param {Array} config.items items representativos entre los cuales se navegara por medio de key event
  * @param {Boolean} config.loader indentifica cuando el componente esta listo para ejecutar la accion de navegar
- * @param {String} config.className clase que identificara los elementos navegables
+ * @param {String} config.uniqueIdForElement id que identificara los elementos navegables
  * @param {Boolean} config.modalRestriction restringe el uso de esta funcion en modales, por defecto es true
  * @param {Event.keyCode|Number} config.prev representa el keyCode a ser referenciendo como previo o anterior, 
  * por defecto es 37
@@ -25,21 +25,21 @@ const DEFAULT_ARGS = {
  * @return _setCurrentSelection
  * 
  * @example 
- * const setCurrentElement = useNavigationKeyActions({ items: [...n], loader: subscription(), className: "my-classname-" }) 
+ * const setCurrentElement = useNavigationKeyActions({ items: [...n], loader: subscription(), uniqueIdForElement: "my-uniqueIdForElement-" }) 
  * @see useItemsInteractions
  * @see window.onkeyup
  */
 export default function useNavigationKeyActions(config) {
     const valuesAsProps = { ...DEFAULT_ARGS, ...config }
-    const { modalRestriction, className, loader, items } = valuesAsProps
+    const { modalRestriction, uniqueIdForElement, loader, items } = valuesAsProps
     const [currentSelection, setCurrentSelection] = useState(-1)
     const isModalVisible = modalRestriction && useSelector(state => state.form.isModalVisible)
-    const isModalRenderShowing = useSelector(state => state.ui.modal.render)
+    const isModalRenderShowing = modalRestriction && useSelector(state => state.ui.modal.render)
 
     useEffect(() => {
         if (items && items.length > 0 && !loader) {
             if (isModalVisible) return
-            const el = document.getElementById(`${className}${valuesAsProps.default}`)
+            const el = document.getElementById(`${uniqueIdForElement}${valuesAsProps.default}`)
             el && el.focus()
         }
     }, [items, loader])
@@ -51,23 +51,23 @@ export default function useNavigationKeyActions(config) {
     }, [isModalRenderShowing, isModalVisible])
 
     useEffect(() => {
-        if (!isModalRenderShowing && !isModalVisible && !window.onkeyup && items && items.length > 0) {
+        if (!isModalRenderShowing && !isModalVisible && !window.onkeyup && items && items.length > 0) {  
             window.onkeyup = (event) => {
                 if (isModalVisible) return
                 const length = valuesAsProps.originalLength ? items.length : items.length - 1
                 const currentSelectionIsDownZero = currentSelection < 0
                 let elementId = 0
-                let el = null
+                let el = null        
                 if (event.keyCode === valuesAsProps.prev) {
                     elementId = currentSelectionIsDownZero ? length : (currentSelection - 1)
-                    el = document.getElementById(`${className}${Math.max(0, elementId)}`)
+                    el = document.getElementById(`${uniqueIdForElement}${Math.max(0, elementId)}`)
                     el && el.focus()
                 }
                 if (event.keyCode === valuesAsProps.next || (event.keyCode === 13 && currentSelectionIsDownZero)) {
                     elementId = currentSelectionIsDownZero ? 0 : (currentSelection + 1)
-                    el = document.getElementById(`${className}${Math.min(length, elementId)}`)
+                    el = document.getElementById(`${uniqueIdForElement}${Math.min(length, elementId)}`)
                     el && el.focus()
-                }
+                }        
             }
         }
         return () => {
