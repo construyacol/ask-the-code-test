da3c146c523c96159f88fbf6053e56147a429d56
douglasacost <douglasacostaaquino@gmail.com>
2020-10-19 21:06:30 -0400

    fix enter on create withdraw account

--- src/components/withKeyActions.js
+++ src/components/withKeyActions.js
@@ -1,10 +1,11 @@
 import React from "react"
 import useKeyActionAsClick from "../hooks/useKeyActionAsClick"
 
-export default function withKeyActions(AsComponent, eventName = 'onkeydown') {
+export default function withKeyActions(AsComponent) {
     return function (props) {
-        const idCancel = useKeyActionAsClick(true, `cancel-button-${eventName}`, 8, true, eventName, true)
-        const idAccept = useKeyActionAsClick(true, `accept-button-${eventName}`, 13, false, eventName, true)
+
+        const idCancel = useKeyActionAsClick(true, `cancel-button-${props.eventName}`, 8, true, props.eventName, true)
+        const idAccept = useKeyActionAsClick(true, `accept-button-${props.eventName}`, 13, false, props.eventName, true)
 
         return (                        
             <AsComponent idCancel={idCancel} idAccept={idAccept}  {...props} />
