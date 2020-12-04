a3447c4cd36c750dadf7c576ee9d0264437094e5
Coinsenda <construyacol@gmail.com>
2020-12-04 11:57:48 -0500

    pair list modal fixed | convert_currency fixed

--- src/utils/convert_currency.js
+++ src/utils/convert_currency.js
@@ -54,7 +54,7 @@ const convertCurrencies = async (currency, amount_spend, pair_id) => {
     objetive_data.to_spend_currency = objetive_pair_instance.secondary_currency;
     //Formateo según la moneda a la que necesito transformar
     let want_to_spend = await formatToCurrency(data.want_to_spend, objetive_pair_instance.secondary_currency, false);
-    objetive_data.want_to_spend = await want_to_spend.multipliedBy(objetive_pair_instance.sell_price).div("1").toFormat();
+    objetive_data.want_to_spend = await want_to_spend.multipliedBy(objetive_pair_instance.sell_price).toFormat();
   } else {
     //Es una compra
     data.want_to_spend = await formatToCurrency(data.want_to_spend, objetive_pair_instance.secondary_currency);
