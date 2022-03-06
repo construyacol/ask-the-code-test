a3d7eef0308b63368f4694136d6d9715d2a4a9d0
andres <construyacol@gmail.com>
2022-03-06 00:37:38 -0500

    onboarding fix

--- src/components/Root.js
+++ src/components/Root.js
@@ -40,7 +40,7 @@ function RootContainer(props) {
   const [tryRestoreSession] = SessionRestore();
   const [toastMessage] = useToastMessage();
   
-  const [ showOnBoarding, setShowOnBoarding ] = useState(true)
+  const [ showOnBoarding, setShowOnBoarding ] = useState(false)
 
   const initComponent = async () => {
     const params = new URLSearchParams(history.location.search);
