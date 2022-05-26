b4559295d37c1c6e017d9874bf677a45192c08c3
andres <construyacol@gmail.com>
2022-05-26 03:37:19 -0500

    identity fix validation

--- src/components/forms/widgets/kyc/identityComponent/identityUtils.js
+++ src/components/forms/widgets/kyc/identityComponent/identityUtils.js
@@ -8,14 +8,14 @@ export const identityInfo = () => {
   // const mainIdentity = user?.identity
   // const rejectedIdentity = ((["rejected"].includes(mainIdentity?.info_state) || ["rejected"].includes(mainIdentity?.file_state)) && mainIdentity?.file_state === mainIdentity?.info_state)
   // if(user?.identities?.length && !rejectedIdentity){
-    pendingIdentityFile = user?.identities.find(identity => ["pending", "rejected"].includes(identity?.file_state));
+  pendingIdentityFile = user?.identities.find(identity => ["pending", "rejected"].includes(identity?.file_state));
   let pendingOrRejectedIdentity = user?.identities.find(identity => ["pending", "rejected"].includes(identity?.file_state) || ["pending", "rejected"].includes(identity?.info_state));
-  let pendingOrConfirmedIdentity = user?.identities.find(identity => ["pending", "confirmed"].includes(identity?.file_state) || ["pending", "confirmed"].includes(identity?.info_state));
+  let confirmedIdentity = user?.identities.find(identity => ["confirmed"].includes(identity?.file_state) && ["confirmed"].includes(identity?.info_state));
   // }
   return {
     pendingIdentityFile,
     pendingOrRejectedIdentity,
-    pendingOrConfirmedIdentity
+    confirmedIdentity
   }
 }
  
