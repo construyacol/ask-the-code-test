0fd89ea831c6fd43a82fb5402c9411500eeaf94f
andres <construyacol@gmail.com>
2022-06-07 00:58:47 -0500

    refactor w  account detail

--- src/components/withdrawAccounts/witdrawAccountContainer.js
+++ src/components/withdrawAccounts/witdrawAccountContainer.js
@@ -14,7 +14,8 @@ import ActivityView from "../wallets/views/activity";
 import { useCoinsendaServices } from "../../services/useCoinsendaServices";
 import { useSelector } from "react-redux";
 import { AccountDetailLayout, AccountDetailContainer } from '../widgets/layoutStyles'
-import TitleSection, { SubTitleSection } from '../widgets/titleSectionComponent'
+// import TitleSection, { SubTitleSection } from '../widgets/titleSectionComponent'
+import { SubTitleSection } from '../widgets/titleSectionComponent'
 import SubMenuComponent from '../menu/subMenu'
 import ItemAccount from "../widgets/accountList/item_account";
 
@@ -133,20 +134,22 @@ const ActivityWrapperView = (props) => {
 
   return (
     <AccountDetailLayout className="_accountDetailLayout">
-      <SubTitleSection 
-        titleKey="Volver a cuentas de retiro"
-        iconClass="fas fa-arrow-left"
-        handleAction={() => props?.history?.push(`/${props?.match?.params?.primary_path}`)}
-      />
+      
 
       <SubMenuComponent
           targetList="withdraw_accounts"
       />
 
-      <TitleSection
+<SubTitleSection 
+        titleKey="Volver a cuentas de retiro"
+        iconClass="fas fa-arrow-left"
+        handleAction={() => props?.history?.push(`/${props?.match?.params?.primary_path}`)}
+      />
+
+      {/* <TitleSection
               className="accoun-detail"
               titleKey={params?.path}
-            />
+            /> */}
       
       <AccountDetailContainer className="_accountDetailContainer">
           <section className="WalletContainer">
