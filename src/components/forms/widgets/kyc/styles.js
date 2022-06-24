3847c0964db100a20868e87b323901c1afeb5055
andres <construyacol@gmail.com>
2022-06-24 16:30:18 -0500

    fix

--- src/components/forms/widgets/kyc/styles.js
+++ src/components/forms/widgets/kyc/styles.js
@@ -1,5 +1,5 @@
 import styled, { keyframes } from 'styled-components'
-import { PRIMARY_COLOR } from '../../const'
+// import { PRIMARY_COLOR } from '../../const'
 
 
 const IconButtonContainer = styled.div`
